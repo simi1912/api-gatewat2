799e02cbfdc4ae8e2d840598d9f5c0ae882899b4
simi1912 <nioasca@gmail.com>
2021-04-19 13:21:13 +0300

    Initial commit

---
+++ src/main/java/com/simi/microservices2/apigatewat2/ApiGatewat2Application.java
@@ -0,0 +1,13 @@
+package com.simi.microservices2.apigatewat2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewat2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewat2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/simi/microservices2/apigatewat2/ApiGatewat2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simi.microservices2.apigatewat2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewat2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
